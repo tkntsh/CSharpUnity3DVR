ccf87c68a2b2e9fc1691c471914ec59318cd1acc
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 18:12:29 +0200

    C# Scripts
    C# scripts to move the camera gameObject into a different sphere

---
+++ SphereChanger2.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SphereChanger : MonoBehaviour
+{
+    public float movementDuration;
+    [SerializeField] Slider volumeSlider;
+
+    public void ChangeSphere(Transform nextSphere)
+    {
+        StartCoroutine(MoveToPosition(nextSphere.position));
+    }
+
+    /*public override void OnWaitDone()
+    {
+    }*/
+
+    public void ChangeCanvas(Transform canvas)
+    {
+        StartCoroutine(MoveToCanvas(canvas.position));
+    }
+
+    public IEnumerator MoveToPosition(Vector3 position)
+    {
+        float elapsedTime = 0;
+        Vector3 initialPosition = transform.position;
+
+        while (elapsedTime < movementDuration)
+        {
+            yield return new WaitForEndOfFrame();
+            elapsedTime += Time.deltaTime;
+
+            transform.position = Vector3.Lerp(initialPosition, position, elapsedTime / movementDuration);
+        }
+        transform.position = position;
+    }
+
+    public IEnumerator MoveToCanvas(Vector3 position)
+    {
+        float elapsedTime = 0;
+        Vector3 initialPosition = transform.position;
+
+        while (elapsedTime < movementDuration)
+        {
+            yield return new WaitForEndOfFrame();
+            elapsedTime += Time.deltaTime;
+
+            transform.position = Vector3.Lerp(initialPosition, position, elapsedTime / movementDuration);
+        }
+        transform.position = position;
+    }
+
+    public void ChangeSpeed()
+    {
+        movementDuration = volumeSlider.value;
+    }
+}
+
---
+++ VRTogOFF.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRTogOFF : MonoBehaviour
+{
+    //TurnOff VR on click
+    
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "VR Split")
+                {
+                    ToggleVR();
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    public void ToggleVR()
+    {
+
+        if (XRSettings.loadedDeviceName == "kardboard")
+        {
+            StartCoroutine(LoadDevice("None"));
+        }
+        else
+        {
+            StartCoroutine(LoadDevice("kardboard"));
+        }
+    }
+
+    IEnumerator LoadDevice(string newDevice)
+    {
+        XRSettings.LoadDeviceByName(newDevice);
+        yield return null;
+        XRSettings.enabled = false;
+    }
+}
